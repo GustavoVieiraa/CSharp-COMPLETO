ded3de799dc54d4eec426c0c8af3fca4f33fcd68
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-01-29 16:25:10 -0300

    Aula 2 - Seção 1 - Finalizada.

---
+++ Seção 1 - Apresentação do Curso e Orientações/o que estudar para me tornar um desenvolvedor.cs
@@ -0,0 +1,19 @@
+/*
+ 
+	Aula 2 - O que estudar para me tornar um desenvolvedor?
+
+	Fundamentos: 
+		1 - Lógica de Programação
+		2 - Estruturas de Dados
+		3 - Programação Orientada a Objetos
+		4 - Banco de Dados
+		5 - Análise & Design de Sistemas
+
+	Comercial:
+		Back-end
+		Web
+		Mobile
+		Desktop
+		Games
+ 
+ */
