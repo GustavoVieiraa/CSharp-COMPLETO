c7926079b751103f536d6384f72568f27c684ac2
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-01-29 16:17:36 -0300

    Aula 1 - Seção 1 - Finalizada

--- Seção 1 - Apresentação do Curso e Orientações/visão-geral-do-curso.cs
+++ Seção 1 - Apresentação do Curso e Orientações/visão-geral-do-curso.cs
@@ -1,1 +1,10 @@
+/*
+            MACROVISÃO DO CURSO
 
+    -> Projetos
+    -> OOP (Programação Orientada a objetos)
+    -> C#
+    -> Tecnologias
+    -> Teoria + Prática: exercícios reais
+
+ */
