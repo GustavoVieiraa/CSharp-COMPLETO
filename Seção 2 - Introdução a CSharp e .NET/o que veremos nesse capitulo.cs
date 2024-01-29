9b660a7c018be5667ca09024c8c655711fc7335a
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-01-29 16:33:55 -0300

    o que veremos nesse capitulo - finalizado.

---
+++ Seção 2 - Introdução a CSharp e .NET/o que veremos nesse capitulo.cs
@@ -0,0 +1,16 @@
+/*
+ 
+ 
+    Conteúdo
+
+    -> C# e .NET
+    -> Compilação e Interpretação
+    -> Estrutura de uma aplicação C# .NEt
+    -> Instalação do Visual Studio no Windows
+    -> Primeiro programa em C# no Visual Studio
+    -> Estrutura de um programa C#
+    -> Dicas do Visual Studio
+ 
+ 
+ 
+ */
