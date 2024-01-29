229a325979f6eb7c7cf8025ab9e47cc3517e14bf
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-01-29 16:42:19 -0300

    CSharp e .NET - finalizado.

---
+++ Seção 2 - Introdução a CSharp e .NET/CSharp e .NET.cs
@@ -0,0 +1,15 @@
+/*
+ 
+	C# e .NET
+
+	C# é uma linguagem de programação (regras sintáticas)
+
+	.NET (2002) é uma plataforma de desenvolvimento para se criar
+	diversos tipos de aplicações, podendo usar várias linguagens de programação.
+	
+	.NET é composto por:
+		-> BCL (base class library)
+		-> CLR (common language runtime) é uma máquina virtual.
+
+ 
+ */
