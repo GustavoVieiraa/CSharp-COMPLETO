3677c10413029b7db1c44d6fa36380b083110a50
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-02-25 17:07:13 -0300

    Ex de fixação utilizando herança e Polimorfismo

---
+++ Pratices/ExCadProducts/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+namespace ExCadProducts.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag()
+        {
+            return Name
+                + " - $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Pratices/ExCadProducts/Entities/UsedProduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExCadProducts.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate)
+            : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " (used) "
+                + "- $ "
+                + Price.ToString(CultureInfo.InvariantCulture)
+                + " (Manufacture date: "
+                + ManufactureDate.ToString("dd/MM/yyyy")
+                + ")";
+        }
+
+
+    }
+}
