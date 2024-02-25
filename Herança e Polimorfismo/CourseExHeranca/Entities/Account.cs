376e6ecd01516b03063706a08e135e10b2069f69
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-02-25 19:56:22 -0300

    Teoria e Prática de Herança em exercício.
    Teoria e Prática de Herança em exercício.

---
+++ Herança e Polimorfismo/CourseExHeranca/Entities/Account.cs
@@ -0,0 +1,43 @@
+﻿namespace CourseExHeranca.Entities
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance{ get; protected set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+
+        public override string ToString()
+        {
+            return
+                "=--------------------= \n"
+                +$"Number account: {Number}"
+                + "\n"
+                + $"Holder: {Holder}"
+                + "\n"
+                + $"Balance: ${Balance}"
+                + "\n=--------------------=";
+        }
+    }
+}
---
+++ Herança e Polimorfismo/CourseExHeranca/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using CourseExHeranca.Entities;
+
+namespace CourseExHeranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(01, "Gustavo", 0);
+            BusinessAccount bacc = new BusinessAccount(02, "GV Developer", 0, 500.0);
+
+            // UPCASTING
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(03, "MegaTron", 50.0, 500.0);
+            Account acc3 = new SavingsAccount(04, "FaceTrue", 0, 300.0);
+
+
+            // DOWNCASTING
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.00);
+
+            // BusinessAccount acc5 = (BusinessAccount)acc3;
+            
+            if (acc3 is BusinessAccount)
+            {
+                // BusinessAccount acc5 = (BusinessAccount)acc3;
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                acc5.Loan(350.00);
+                Console.WriteLine(acc5);
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                // SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine($"Good -> \n{acc5}");
+            }
+        }
+    }
+}  
