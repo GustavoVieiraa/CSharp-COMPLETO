dcb22e242863025160a98cae83549e8c6e0bc997
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-02-25 19:53:58 -0300

    Ex Prático e Teorico sobre métodos abstratos

---
+++ Herança e Polimorfismo/ExMetodosAbstratos/Entities/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using ExMetodosAbstratos.Entities.Enums;
+
+
+namespace ExMetodosAbstratos.Entities
+{
+    abstract class Shape
+    {
+        public Color Color { get; set; }
+
+
+        // public Shape() { }
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();
+        
+
+    }
+}
