ca04e0730dccd8d1dd6cb24bb39e65575ef5c971
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-02-25 22:33:32 -0300

    Prática e Teoria da estrutura TryCatch e Finally

---
+++ Tratamento de exceções/EstruturaBlocoFinally/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace EstruturaBlocoFinally
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            FileStream fs = null;
+
+            try
+            {
+                fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+
+        }
+    }
+}
