72bda0af4d6ff6ade7b8b43c912c6cb476cbe620
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-02-29 19:39:46 -0300

    Prática e teórica de manipulação de arquivos.

---
+++ Trabalhando com Arquivos/File-FileInfo-IOException/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+
+namespace CourseFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sourcePath = @"C:\test\file1.txt";
+            string targetPath = @"C:\test\file2.txt";
+
+            try
+            {
+                FileInfo fileinfo = new FileInfo(sourcePath);
+                fileinfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
