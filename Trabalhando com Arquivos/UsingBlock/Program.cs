57ba7a5eb41352c36e49362c6c0c45a5e7a26698
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-02-29 20:36:25 -0300

    Parte teórica e prática sobre Using Block

---
+++ Trabalhando com Arquivos/UsingBlock/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+/*
+ 
+                ### Using Block
+    Sintaxe simplificada que garante que os objetos
+    IDisposable serão fechados.
+
+    Objetos IDisposable NÃO são gerenciados pelo CLR.
+    Eles precisam ser manualmente fechados.
+
+    Exemplos: Font, FileStream, StreamReader, StreamWriter.
+ 
+ */
+
+
+namespace FileUsingBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\test\file1.txt";
+
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            
+        }
+    }
+}
