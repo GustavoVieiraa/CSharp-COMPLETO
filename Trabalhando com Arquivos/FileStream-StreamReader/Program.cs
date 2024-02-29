7d10aa8eaafe2152a0b0b0c213c993175934a7c0
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-02-29 20:06:57 -0300

    Parte teórica de FileStream e StreamReader e aplicando em exemplos.
    Aplicando FileStream para instanciar um arquivo através do objeto File e instanciando StreamReader para acessar o arquivo.

---
+++ Trabalhando com Arquivos/FileStream-StreamReader/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace CourseFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string file = @"C:\test\file1.txt";
+
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(file);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error accurred: ");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+            }
+
+        }
+    }
+}
