1e0558c3b7594292742e26aab48284560fe79087
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-03-03 17:52:11 -0300

    Exercício de Fixação Trabalhando com Arquivos
    Nesse exercício, apliquei toda a parte teórica criando um arquivo a partir de uma entrada do usuário e retirei dados de um arquivo csv. Após retirados os dados, criei uma nova pasta de saída (out) e criei um novo arquivo csv com os dados tratados conforme solicitado.

---
+++ Trabalhando com Arquivos/ExFixaçãoTrabalhandoComArquivos/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace ExFixaçãoTrabalhandoComArquivos.Entities
+{
+    internal class Product
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+
+        public Product()
+        {
+        }
+
+        public Product(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+
+        public double Total()
+        {
+            return Preco * Quantidade;
+        }
+
+    }
+}
---
+++ Trabalhando com Arquivos/ExFixaçãoTrabalhandoComArquivos/Program.cs
@@ -0,0 +1,64 @@
+﻿using ExFixaçãoTrabalhandoComArquivos.Entities;
+using System;
+using System.IO;
+using System.Globalization;
+
+/*
+ 
+            ### Exercício - Trabalhando com Arquivos by Gusta x:II
+
+    Fazer um programa para ler o caminho de um arquivo .csv contendo os dados
+    de itens vendidos. Cada item possui um nome, preço unitário e quantidade,
+    separados por vírgula. Você deve gerar um novo arquivo chamado "summary.csv",
+    localizado em uma subpasta chamada "out" a partir da pasta original do arquivo
+    de origem, contendo apenas o nome e o valor total para aquele item (preço
+    unitário multiplicado pela quantidade).
+ 
+ */
+
+
+namespace ExArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Entre com o caminho da pasta: ");
+            string dadosArquivo = Console.ReadLine();
+
+            try
+            {
+                string[] linhas = File.ReadAllLines(dadosArquivo);
+                string pastaInicial = Path.GetDirectoryName(dadosArquivo);
+                string novaPastaOut = pastaInicial + @"\out";
+                string arquivoSaida = novaPastaOut + @"\summary.txt";
+                
+                Directory.CreateDirectory(novaPastaOut);
+
+                using (StreamWriter sw = File.AppendText(arquivoSaida))
+                {
+                    foreach (string line in linhas)
+                    {
+                        string[] fields = line.Split(',');
+                        string nome = fields[0];
+                        double preco = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantidade = int.Parse(fields[2]);
+
+                        Product product = new Product(nome, preco, quantidade);
+
+                        sw.WriteLine($"{product.Nome}, {product.Total().ToString("F2", CultureInfo.InvariantCulture)}");
+                    }
+                }
+
+                
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Erro: {e.Message}");
+            }
+
+        }
+    }
+}
