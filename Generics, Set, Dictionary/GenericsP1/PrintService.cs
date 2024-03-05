62cba37338743dac493f3fa7ab460dfe25484872
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-03-05 07:35:21 -0300

    Parte prática e teórica de Generics - Parte 1

---
+++ Generics, Set, Dictionary/GenericsP1/PrintService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GenericsP1
+{
+    class PrintService
+    {
+        private int[] _values = new int[10];
+        private int _count = 0;
+
+        public void AddValue(int value)
+        {
+            if(_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is Full!");
+            }
+            
+            _values[_count] = value;
+            _count++;
+        }
+
+        public int First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _values[0];
+        }
+
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write($"{_values[i]}, ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+
+    }
+}
---
+++ Generics, Set, Dictionary/GenericsP1/Program.cs
@@ -0,0 +1,41 @@
+﻿using GenericsP1;
+using System;
+using System.IO;
+
+/*
+ 
+            ### Problema Motivador - by Gusta : x:II ###
+
+    Deseja-se fazer um programa que leia um conjunto
+    N de números inteiros (N de 1 a 10), e depois imprima
+    esses números de forma organizada. E também, informar
+    qual foi o primeiro valor informado.
+ 
+ */
+
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                printService.AddValue(value);
+            }
+
+            printService.Print();
+
+            Console.WriteLine($"First: {printService.First()}");
+
+        }
+    }
+}
