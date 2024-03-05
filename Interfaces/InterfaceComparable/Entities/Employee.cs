849518e6b9fbae857ae31501570101f44d501d01
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-03-04 22:02:24 -0300

    Prática e teórica sobre a interface IComparable

---
+++ Interfaces/InterfaceComparable/Entities/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace InterfaceComparable.Entities
+{
+    internal class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee() { }
+
+        public Employee(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Salary.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: argument is not an Employee");
+            }
+
+            Employee other = obj as Employee;
+
+            return Name.CompareTo(other.Name);
+
+        }
+    }
+}
---
+++ Interfaces/InterfaceComparable/Program.cs
@@ -0,0 +1,49 @@
+﻿using InterfaceComparable.Entities;
+using System;
+using System.IO;
+
+/*
+ 
+            ### Problema Motivador
+    Faça um programa para ler um arquivo contendo
+    nomes de pessoas (um nome por linha), armazenando-os
+    em uma lista. Depois, ordenar os dados dessa lista e 
+    mostra-los ordenadamente na tela. Nota: o caminho
+    do arquivo pode ser informado "hardcode".
+ 
+ */
+
+namespace InterfaceComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFile = @"C:\dados\info1.txt";
+
+            try
+            {
+
+                using (StreamReader sr = File.OpenText(myFile)) 
+                {
+                    List<Employee> employees = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        employees.Add(new Employee(sr.ReadLine()));
+                    }
+                    employees.Sort();
+                    foreach (Employee employee in employees)
+                    {
+                        Console.WriteLine(employee);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+        }
+    }
+}
