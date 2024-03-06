098eccd43bb753975364aa269a4b13ab1885bf22
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-03-06 11:19:28 -0300

    Parte prática e teórica aplicando uma lista com objetos utilizando Generics

---
+++ Generics, Set, Dictionary/RestricoesDeGenerics/Program.cs
@@ -0,0 +1,57 @@
+﻿using RestricoesDeGenerics.Entities;
+using System;
+using System.Globalization;
+
+/*
+ 
+                ### Restrições de Generics
+    Uma empresa de consultoria deseja avaliar a performance
+    de produtos, funcionários, dentre outras coisas.
+    Um dos cálculos que ela precisa é encontrar o maior
+    dentre um conjunto de elementos. Fazer um programa
+    que leia um conjunto de N produtos e depois mostre
+    o mais caro deles.
+ 
+ */
+
+namespace Generics
+{
+    class Program()
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                List<Product> list = new List<Product>();
+                Console.Write("Enter N: ");
+                int n = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < n; i++)
+                {
+                    string[] vect = Console.ReadLine().Split(",");
+                    string name = vect[0];
+                    double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                    list.Add(new Product(name, price));
+                }
+
+                CalculationService calculationService = new CalculationService();
+
+                Product max = calculationService.Max(list);
+
+                Console.WriteLine($"Max: {max}");
+
+
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Erro: {e.Message}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Erro: {e.Message}");
+            }
+        }
+    }
+}
---
+++ Generics, Set, Dictionary/RestricoesDeGenerics/Service/CalculationService.cs
@@ -0,0 +1,26 @@
+﻿namespace Generics
+{
+     class CalculationService
+    {
+        
+        public T Max<T>(List<T> list) where T : IComparable
+        {
+            if (list.Count == 0)
+            {
+                throw new ArgumentException("The list can not be empty.");
+            }
+
+            T max = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i].CompareTo(max) > 0f)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+
+        }
+
+    }
+}
