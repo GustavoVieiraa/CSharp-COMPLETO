f8dbd617ba7f2b0a923b69bb1e41d5b46c544c74
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-03-06 08:35:26 -0300

    Parte teórica e prática aplicando Type Generics

---
+++ Generics, Set, Dictionary/GenericsP2/PrintService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GenericsP2
+{
+    /* Tipagem T, parametrização para utilizar tipagem (type safety & performance)*/
+    class PrintService <T>
+    {
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if(_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is Full!");
+            }
+            
+            _values[_count] = value;
+            _count++;
+        }
+
+        public T First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _values[0];
+        }
+
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write($"{_values[i]}, ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+
+    }
+}
---
+++ Generics, Set, Dictionary/GenericsP2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+/*
+ 
+            ### Problema Motivador - by Gusta : x:II ###
+
+    Deseja-se fazer um programa que leia um conjunto
+    N de números inteiros (N de 1 a 10), e depois imprima
+    esses números de forma organizada. E também, informar
+    qual foi o primeiro valor informado.
+ 
+ */
+
+
+namespace GenericsP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // PrintService printService = new PrintService();
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                printService.AddValue(value);
+            }
+
+            int a = printService.First();
+
+            Console.WriteLine(b);
+
+            printService.Print();
+
+            Console.WriteLine($"First: {printService.First()}");
+
+        }
+    }
+}
