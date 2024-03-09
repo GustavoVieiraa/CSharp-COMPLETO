f23f54c812ddd0035ad82c48733d54604fd469a9
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-03-09 11:07:59 -0300

    Parte prática e teórica trabalhando com GetHashCode & Equals

---
+++ Generics, Set, Dictionary/GetHashCode-Equals/Entities/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Client() {}
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Client))
+            {
+                return false;
+            }
+            Client other = obj as Client;
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+
+    }
+}
---
+++ Generics, Set, Dictionary/GetHashCode-Equals/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Entities;
+
+/*
+ 
+            ### GetHashCode & Equals ###
+    ~ São operações da classe Object utilizadas
+    para comparar se um objeto é igual ao outro.
+    
+    ~ Equals: Lento, resposta 100%.
+    ~ GetHashCode: Rápido, porém resposta postiva não é 100%
+
+    Os tipos pré-definidos já possuem implementação para essas
+    operações. Classes e structs personalizados precisam
+    sobrepô-las.
+ 
+ */
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Client a = new Client { Name = "Gustavo", Email = "gusta@gmail.com" };
+            Client b = new Client { Name = "Rafael", Email = "gusta@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+        }
+    }
+}
