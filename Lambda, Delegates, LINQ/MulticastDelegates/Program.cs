6a320701259a509c85d3b46c40882e68fc86c763
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-04-13 11:02:14 -0300

    Teórica e prática de Multicast Delegates

---
+++ Lambda, Delegates, LINQ/MulticastDelegates/Program.cs
@@ -0,0 +1,40 @@
+﻿using MulticastDelegates.Services;
+using System;
+
+
+/*
+                          Multicast Delegates
+
+        Delegates que guardam a referência para mais de um método
+        
+        Para adicionar uma referência, pode-se usar o operador +=
+
+        A chamada Invoke (ou sintaxe reduzida) executa todos os métodos
+        na ordem em que foram adicionados.
+
+        Seu uso faz sentido para métodos void
+
+ */
+
+
+namespace Course
+{
+
+    delegate void BinaryNumericoOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double a = 18;
+            double b = 25;
+
+            BinaryNumericoOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+
+        }
+    }
+}
---
+++ Lambda, Delegates, LINQ/MulticastDelegates/Services/CalculationService.cs
@@ -0,0 +1,17 @@
+﻿namespace MulticastDelegates.Services
+{
+    class CalculationService
+    {
+        public static void ShowMax(double x, double y)
+        {
+            double max = (x > y) ? x : y;
+            Console.WriteLine(max);
+        }
+
+        public static void ShowSum(double x, double y)
+        {
+            double sum = x + y;
+            Console.WriteLine(sum);
+        }
+    }
+}
