061f513d4f603f3f868c893b7fd6e300c7e13f64
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-04-13 10:36:13 -0300

    Introdução a Delegates e sua aplicação como Type

---
+++ Lambda, Delegates, LINQ/IntroductionDelegates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using IntroductionDelegates.Services;
+
+/*
+    
+                Introdução Delegates
+
+    É uma referência (com type safety) para um ou mais métodos
+        É um tipo referência
+
+    Usos comuns:
+        Comunicação entre objetos de forma flexível e extensível (eventos / callbacks)
+        Parametrização de operações por métodos (programação funcional)
+ 
+ */
+
+
+namespace Course
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 25;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Max;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
---
+++ Lambda, Delegates, LINQ/IntroductionDelegates/Services/CalculationService.cs
@@ -0,0 +1,22 @@
+﻿namespace IntroductionDelegates.Services
+{
+     class CalculationService
+    {
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x : y;
+        }
+
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Square(double x)   
+        {
+            return x * x;
+        }
+
+
+    }
+}
