b607216980b7dc325e84492e4b646543a74fe960
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-04-13 12:29:32 -0300

    Teórica e Prática aplicando Predicate utilizando RemoveAll

---
+++ Lambda, Delegates, LINQ/Predicate/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Course.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} = {Price.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
---
+++ Lambda, Delegates, LINQ/Predicate/Program.cs
@@ -0,0 +1,47 @@
+﻿using Course.Entities;
+using System;
+
+/*          
+ 
+            Predicate (System)
+    
+    Representa um método que recebe um objeto
+    do tipo T e retorna um valor booleano
+
+            Problema exemplo
+
+    Fazer um programa que, a partir de uma lista
+    de produtos, remova da lista somente aqueles
+    cujo preço mínimo seja 100.
+
+
+ */
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("TV LG", 900.00));
+            products.Add(new Product("Mouse Razer", 50.00));
+            products.Add(new Product("Tablet Samsung", 350.50));
+            products.Add(new Product("SSD", 80.90));
+
+            products.RemoveAll(ProductTest);
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0;
+        }
+    }
+}
