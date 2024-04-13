69c444999ee73a364f7201b5af66f78259f2432c
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-04-13 20:06:48 -0300

    Prática e Teórica utilizando o delegate Action

---
+++ Lambda, Delegates, LINQ/Action/Action/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Course.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} = R${Price.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
---
+++ Lambda, Delegates, LINQ/Action/Action/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Course.Entities;
+
+/*
+                Action (System)
+    
+    Representa um método void que recebe zero
+    ou mais argumentos.
+
+                Problema Exemplo
+    
+    Fazer um programa que, a partir de uma lista
+    de produtos, aumento o preço dos produtos em
+    10%.
+ 
+ */
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("TV Samsung", 900.00));
+            products.Add(new Product("Mouse Razer", 50.00));
+            products.Add(new Product("Tablet Samsung", 350.50));
+            products.Add(new Product("SSD Kingston", 80.90));
+
+            /*Action<Product> act = prod => { prod.Price += prod.Price * 0.1; };*/
+
+
+            products.ForEach(prod => { prod.Price += prod.Price * 0.1; });
+            foreach (Product p in products)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        /*static void UpdatePrice(Product prod)
+        {
+            prod.Price += prod.Price * 0.1;
+        }*/
+
+    }
+}
