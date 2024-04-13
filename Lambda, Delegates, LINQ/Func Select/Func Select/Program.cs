6e1bc19a4fbf69635ff04721dd63005cfa0289ac
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-04-13 20:23:32 -0300

    Prática e Teórica utilizando Func com Select

---
+++ Lambda, Delegates, LINQ/Func Select/Func Select/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Course.Entities;
+
+/*
+                Func (System)
+
+    Representa um método que recebe zero
+    ou mais argumentos, e retorna um valor.
+ 
+ */
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("TV lG", 900.00));
+            products.Add(new Product("Mouse DragonRed", 50.00));
+            products.Add(new Product("Tablet Xiaomi", 350.50));
+            products.Add(new Product("SSD KingsTags", 80.90));
+
+            /*Func<Product, string> resultProducts = p => p.Name.ToUpper();*/
+
+            List<string> result = products.Select(p => p.Name.ToUpper()).ToList();
+
+            foreach (string product in result)
+            {
+                Console.WriteLine(product);
+            }
+
+        }
+
+        /*static string ReturnNameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }*/
+
+    }
+}
